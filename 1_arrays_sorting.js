32fb7e1f2bd8b7dde4f79a1d68abc5a21a7a3a78
lunarctic <158510510+lunarctic@users.noreply.github.com>
2024-02-28 13:21:31 +0100

    changes

--- 1_arrays_sorting.js
+++ 1_arrays_sorting.js
@@ -1,8 +1,13 @@
 /*
  * Sorterings-funksjoner:
  */
+/* med sort funksjonen henter den inn alltid to elementer, i dette tilfellet to kort*/
 function sammenliknKortEtterTall(kortA, kortB) {
+    console.log("kortA: " + kortA)
+    console.log("kortB: " + kortB)
+    console.log(kortA)
     tallA = parseInt(kortA) // Henter ut tallverdien av kort a
+    console.log(tallA)
     tallB = parseInt(kortB) // Henter ut tallverdien av kort b
     // Vil at Ess teller som høyeste kort:
     if (tallA == 1) {
@@ -11,6 +16,19 @@ function sammenliknKortEtterTall(kortA, kortB) {
     if (tallB == 1) {
         tallB = 14
     }
-    return tallA - tallB
+    console.log("the difference: " + (tallA - tallB))
+    return tallA - tallB /* det blir plutselig motsatt sorteringsvei når man endrer tallA og tallB*/
 }
+
+
+// Skal sammenligne strenger som ser slik ut:
+// "1_of_clubs"
+// "12_of_spades"
+function sammenliknKortEtterTallOgType(kortA, kortB){
+    
+}
+function sammenliknKortEtterTekst (kortA, kortB){
+    return kortA.localeCompare(kortB)
+}
+
 
